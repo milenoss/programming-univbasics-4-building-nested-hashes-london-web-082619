a127230f2bb5b27e868361a5b68e1ee5797c1e17
Milen Khanal <milenkhanal@gmail.com>
2019-08-06 19:22:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,8 +58,8 @@ def monopoly_with_fourth_tier
         :b_and_o_railroad => {
                    "mortgage_value" => "$400"},
         :shortline_railroad =>{ 
-        "mortgage_value" => "$800
-        "}
+        "mortgage_value" => "$800"
+        }
     }
    }
         
